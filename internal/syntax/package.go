875f2b8a45433aa0364131fe18610318654cf5ab
i10416 <ito.yo16uh90616@gmail.com>
2025-05-07 03:10:18 +0900

    tweak: piano syntax helpers

--- internal/syntax/package.go
+++ internal/syntax/package.go
@@ -4,10 +4,6 @@
 package syntax
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
 	"terraform-provider-piano/internal/piano"
 
@@ -15,21 +11,7 @@ import (
 )
 
 func AnyResponseFrom(response *http.Response, diagnostics *diag.Diagnostics) (*piano.AnyResponse, error) {
-	defer response.Body.Close()
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		diagnostics.AddError("IO Error", fmt.Sprintf("Unable to read licensee, got error: %s", err))
-		return nil, err
-	}
-	anyResponse := piano.AnyResponse{}
-	err = json.Unmarshal(body, &anyResponse)
-	if err != nil {
-		diagnostics.AddError("Decode Error", fmt.Sprintf("Unable to decode piano AnyMessage, got error: %s", err))
-		return nil, err
-	}
-	if anyResponse.Code != 0 {
-		diagnostics.AddError(fmt.Sprintf("Status Error: %d: %s", anyResponse.Code, *anyResponse.Message), string(anyResponse.Raw))
-		return nil, errors.New("status error")
-	}
-	return &anyResponse, err
+	return piano.SuccessfulResponseFrom(response, func(summary, detail string) {
+		diagnostics.AddError(summary, detail)
+	})
 }
